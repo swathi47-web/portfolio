0bb273ae202759beefa778ca48efcb3b7757c869
Swathi <illuruswathi22@gmail.com>
2025-03-02 23:54:25 +0530

    Initial commit - Portfolio website

---
+++ task/1.html/script.js
@@ -0,0 +1,11 @@
+document.getElementById('emoji-btn').addEventListener('click', function() {
+  alert('Emojis are coming soon!');
+});
+
+document.getElementById('audio-call-btn').addEventListener('click', function() {
+  alert('Audio call feature is in development.');
+});
+
+document.getElementById('video-call-btn').addEventListener('click', function() {
+  alert('Video call feature is in development.');
+});
